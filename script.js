1214b9249a3813416db1d002d756d19379644466
Kush Knsl <mrkushkansal@gmail.com>
2024-02-19 10:34:41 +0530

    GameArea is ready. Rules page added

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-const timer= document.getElementById("Timer");
-document.getElementById("start").addEventListener("click", startGame); 
-document.getElementById("stop").addEventListener("click", endGame); 
-var continueTimer=false;
-function startGame()
-{
-    document.getElementById("start").disabled=true;
-    document.getElementById("stop").disabled=false;
-    continueTimer=true;
-    timer_start();
-    initialise_game();
+const timer = document.getElementById("Timer");
+document.getElementById("start").addEventListener("click", startGame);
+document.getElementById("stop").addEventListener("click", endGame);
+let continueTimer = false,
+  flag = 1;
+function startGame() {
+  document.getElementById("start").disabled = true;
+  document.getElementById("stop").disabled = false;
+  continueTimer = true;
+  if (flag == 1) timer_start();
+  flag = 0;
+  initialise_game();
 }
-function endGame()
-{
-    document.getElementById("start").disabled=false;
-    document.getElementById("stop").disabled=true;
-    continueTimer=false;
+function endGame() {
+  document.getElementById("start").disabled = false;
+  document.getElementById("stop").disabled = true;
+  continueTimer = false;
+  let tiles=document.querySelectorAll(".tile");
+    for(let i=0;i<tiles.length;i++)
+    {
+        tiles[i].disabled=true;
+    }
+    tiles=document.querySelectorAll(".tile-sol");
+    for(let i=0;i<tiles.length;i++)
+    {
+        tiles[i].disabled=true;
+    }
 }
-var count=0;
-function timer_start()
-{
-    setInterval(()=>{
-        if(!continueTimer)
-        {
-            count=0;
-        }
-        else timer.innerText=(++count)+"s";
-    },1000)   
+var count = 0;
+function timer_start() {
+  setInterval(() => {
+    if (!continueTimer) {
+      count = 0;
+    } else timer.innerText = count++ + "s";
+  }, 1000);
 }
-var previousQ=0;
-function sampleQ()
-{
-const unsolvedSudoku1 = [
+var previousQ = 0;
+function sampleQ() {
+  const unsolvedSudoku1 = [
     [5, 3, 0, 0, 0, 7, 0, 0, 0],
     [6, 0, 0, 1, 9, 5, 0, 0, 0],
     [0, 9, 8, 0, 0, 0, 0, 6, 0],
@@ -63,7 +69,7 @@ const unsolvedSudoku1 = [
     [4, 0, 0, 0, 1, 0, 8, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ];
-const unsolvedSudoku4 = [
+  const unsolvedSudoku4 = [
     [0, 2, 0, 8, 0, 0, 0, 0, 1],
     [8, 0, 0, 0, 0, 0, 2, 0, 0],
     [0, 0, 0, 0, 1, 9, 4, 8, 0],
@@ -85,35 +91,71 @@ const unsolvedSudoku4 = [
     [0, 0, 6, 3, 0, 0, 0, 7, 0],
     [2, 0, 0, 0, 8, 0, 3, 0, 0],
   ];
-  let random = Math.round((Math.random()*5));
-  if(random==previousQ) return sampleQ();
-  else previousQ=random;
-  if(random==1) return unsolvedSudoku1;
-  else if(random==2) return unsolvedSudoku2;
-  else if(random==3) return unsolvedSudoku3;
-  else if(random==4) return unsolvedSudoku4;
+  let random = Math.round(Math.random() * 5);
+  if (random == previousQ) return sampleQ();
+  else previousQ = random;
+  if (random == 1) return unsolvedSudoku1;
+  else if (random == 2) return unsolvedSudoku2;
+  else if (random == 3) return unsolvedSudoku3;
+  else if (random == 4) return unsolvedSudoku4;
   else return unsolvedSudoku5;
 }
-function initialise_game()
+function initialise_game() 
 {
-    let arr=sampleQ();
-    const gameArea=document.querySelector(".SudokuBox-container");
-    gameArea.innerHTML="";
+    let tilesol=document.querySelectorAll(".tile-sol");
+    for(let i=0;i<tilesol.length;i++)
+    {
+        tilesol[i].disabled=false;
+    }
+  let arr = sampleQ();
+  const gameArea = document.querySelector(".SudokuBox-container");
+  gameArea.innerHTML = "";
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      let tile = document.createElement("button");
+      tile.id = "" + i + "-" + j;
+      tile.classList.add("tile");
+      if (arr[i][j] != 0) {
+        tile.innerText = arr[i][j];
+        tile.classList.add("filled");
+        tile.disabled=false;
+      }
+      else tile.classList.add("un-filled");
+      if ((j + 1) % 3 == 0) tile.classList.add("right-dark-border");
+      if ((i + 1) % 3 == 0) tile.classList.add("down-dark-border");
+      gameArea.append(tile);
+    }
+  }
+  solving(arr);
+}
+function solving(array) 
+{
+    let solTiles=document.querySelectorAll(".tile-sol");
+    let prevsolTile=null;
     for(let i=0;i<9;i++)
     {
-        for(let j=0;j<9;j++)
+        solTiles[i].addEventListener("click", (event)=>
         {
-            let tile = document.createElement("button");
-            tile.id = ""+i+"-"+j;
-            tile.classList.add("tile");
-            if(arr[i][j]!=0)
-            {
-                tile.innerText=arr[i][j];
-                tile.classList.add("filled");
-            }
-            if((j+1)%3==0)  tile.classList.add("right-dark-border");
-            if((i+1)%3==0)  tile.classList.add("down-dark-border");
-            gameArea.append(tile);
-        }
+            console.log(event.target);
+            let solSelected=event.target.id;
+            if(prevsolTile != null)
+            prevsolTile.classList.remove("selected");
+            event.target.classList.add("selected");
+            prevsolTile=event.target;
+        });    
+    }
+    let prevTile=null;
+    let tiles=document.querySelectorAll(".un-filled");
+    for(let i=0;i<tiles.length;i++)
+    {
+        tiles[i].addEventListener("click", (event)=>{
+            console.log(event.target);
+            let tileSelected=event.target.id;
+            console.log(tileSelected);
+            if(prevTile != null)
+            prevTile.classList.remove("selected");
+            event.target.classList.add("selected");
+            prevTile=event.target;
+        });    
     }
 }
